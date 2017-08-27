bfe17c8a5aa38677425d555a51c32d07b70bd6d9
Amr Salman <iamrsalman@gmail.com>
2017-08-27 06:05:19 +0200

    Constants

---
+++ iASTwitter/Constants.swift
@@ -0,0 +1,34 @@
+//
+//  Constants.swift
+//  iASTwitter
+//
+//  Created by Amr Salman on 8/26/17.
+//  Copyright © 2017 Amr Salman. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+  static let consumerKey = "VWfofGlVvdRhgvpVIYQqrgp0w"
+  static let consumerSecret = "3Br4YLjbFMFLgEa9wnGLdrRakchSpBcbzOmvjGdgrUlUoKeb4W"
+}
+
+struct Keys {
+  static let id = "id_str"
+  static let userID = "user_id"
+  static let username = "screen_name"
+  static let token = "token"
+  static let name = "name"
+  static let profileImageURL = "profile_image_url_https"
+  static let users = "users"
+  static let description = "description"
+  static let cursor = "cursor"
+  static let backgroundImageURL = "profile_background_image_url_https"
+  static let count = "count"
+}
+
+struct Storyboards {
+  static let login = "Login"
+  static let followers = "Followers"
+  static let followerDetails = "FollowerDetails"
+}
