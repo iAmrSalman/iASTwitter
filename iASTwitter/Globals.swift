145ed7e86ff906de17c85c0d91b05dcffb2f854a
Amr Salman <iamrsalman@gmail.com>
2017-08-27 06:06:47 +0200

    Typealias for json

---
+++ iASTwitter/Globals.swift
@@ -0,0 +1,13 @@
+//
+//  Globals.swift
+//  iASTwitter
+//
+//  Created by Amr Salman on 8/26/17.
+//  Copyright © 2017 Amr Salman. All rights reserved.
+//
+
+import Foundation
+
+//MARK: - Typealias
+
+typealias JSON = [String: Any]
