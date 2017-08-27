158a9020da2fa4d76db801f0599012ef8bd56a4c
Amr Salman <iamrsalman@gmail.com>
2017-08-27 06:03:54 +0200

    Added base view controller

---
+++ iASTwitter/BaseViewController.swift
@@ -0,0 +1,28 @@
+//
+//  BaseViewController.swift
+//  iASTwitter
+//
+//  Created by Amr Salman on 8/18/17.
+//  Copyright © 2017 Amr Salman. All rights reserved.
+//
+
+import UIKit
+
+class BaseViewController: UIViewController {
+  
+  //MARK: - Properties
+  
+  //MARK: - Life Cycle
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    setup()
+  }
+  
+  //MARK: - Helpers
+  
+  func setup() {
+    
+  }
+}
